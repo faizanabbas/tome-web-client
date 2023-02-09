ed93130616afc71a0056953832c3d58124b0913f
Faizan Abbas <faizan@faizanabbas.me>
2023-02-09 16:04:41 +0000

    Initial commit: Project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tome Web Client
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="container mt-4 mx-4">
+        <h1 className="text-3xl font-bold">Tome</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
