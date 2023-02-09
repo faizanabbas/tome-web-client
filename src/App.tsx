27971b6206be574a8b912422e4ba0dbafbefd4cd
Faizan Abbas <faizan@faizanabbas.me>
2023-02-09 16:38:38 +0000

    Add search box

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 function App() {
   return (
     <div className="App">
-      <div className="container mt-4 mx-4">
-        <h1 className="text-3xl font-bold">Tome</h1>
+      <div className="container mt-4 mx-auto px-4 max-w-5xl">
+        <div className="flex gap-4">
+          <h1 className="text-3xl font-bold">Tome</h1>
+          <input
+            className="w-full rounded-full border-2 border-slate-500/10 px-3 outline-none"
+            type="text"
+            placeholder="Search for a book..."
+          />
+        </div>
       </div>
     </div>
   )
