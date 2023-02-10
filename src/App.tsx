0f8f9877c07b82def423fa18d4f1c1323f195b2d
Faizan Abbas <faizan@faizanabbas.me>
2023-02-10 22:01:47 +0000

    Display search results in UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import { FormEventHandler, useState } from 'react'
+import defaultThumbnail from './assets/no_cover_thumb.gif'
+
+interface IVolume {
+  id: string
+  volumeInfo: {
+    title: string
+    imageLinks?: { thumbnail: string }
+    previewLink: string
+  }
+}
 
 function App() {
   const [searchQuery, setSearchQuery] = useState<string>('')
+
+  const [searchResults, setSearchResults] = useState<IVolume[]>([])
 
   const submitHandler: FormEventHandler = async (
     e: React.FormEvent<HTMLInputElement>,
@@ -9,7 +21,10 @@ function App() {
     e.preventDefault()
     await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchQuery}`)
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        console.log(data)
+        setSearchResults(data.items)
+      })
   }
 
   return (
@@ -26,6 +41,27 @@ function App() {
               onChange={(e) => setSearchQuery(e.currentTarget.value)}
             />
           </form>
+        </div>
+      </div>
+      <div className="container mt-8 mx-auto px-4">
+        <div className="flex flex-wrap gap-2 justify-center">
+          {searchResults.map((searchResult) => {
+            return (
+              <div className="p-4 rounded" key={searchResult.id}>
+                <a href={searchResult.volumeInfo?.previewLink} target="_blank">
+                  <img
+                    className="mx-auto rounded w-40"
+                    src={
+                      typeof searchResult.volumeInfo.imageLinks === 'undefined'
+                        ? defaultThumbnail
+                        : searchResult.volumeInfo.imageLinks.thumbnail
+                    }
+                    alt={searchResult.volumeInfo?.title}
+                  />
+                </a>
+              </div>
+            )
+          })}
         </div>
       </div>
     </div>
