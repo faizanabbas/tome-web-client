7c624d00a4a4a961bf378ad8386cad95cb712e9a
Faizan Abbas <faizan@faizanabbas.me>
2023-02-17 03:32:21 +0000

    Remove unused code from Search.tsx

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import { IVolume } from '../../@types/search'
 import SearchContextProvider from '../../context/SearchContext'
 import SearchBar from './SearchBar'
 import SearchResultList from './SearchResultList'
 
 const Search = () => {
-  const [searchResults, setSearchResults] = useState<IVolume[]>([])
-
   return (
     <div className="w-full border-2 border-slate-500/10 rounded">
       <SearchContextProvider>
