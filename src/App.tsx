a210377d72026d80c242b06a370e92578837260b
Faizan Abbas <faizan@faizanabbas.me>
2023-03-03 11:31:55 +0000

    Blank slate

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,3 @@
-import BookList from './components/BookList/BookList'
-import Header from './components/Header'
-import BookListProvider from './context/BookListContext'
-
-function App() {
-  return (
-    <BookListProvider>
-      <div className="App h-screen dark:bg-black dark:text-white mt-0 px-4">
-        <div className="container relative mx-auto py-4 px-4 max-w-5xl">
-          <Header />
-          <div className="absolute top-24 left-0 right-0 w-full">
-            <BookList />
-          </div>
-        </div>
-      </div>
-    </BookListProvider>
-  )
+export default function App() {
+  return <>Tome</>
 }
-
-export default App
