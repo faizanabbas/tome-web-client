d008e67d70d11e4c43ad35c112cabba9a7b264c9
Faizan Abbas <faizan@faizanabbas.me>
2023-03-05 12:12:56 +0000

    Add method to search for a book in Search Context

--- src/context/SearchContext.tsx
+++ src/context/SearchContext.tsx
@@ -6,19 +6,22 @@ import {
   useState,
 } from 'react'
 import { IBook } from '../@types/book'
+import { performSearch } from '../api/api'
 
 interface ISearchContext {
   query: string
   setQuery: Dispatch<SetStateAction<string>>
   results: IBook[]
   setResults: Dispatch<SetStateAction<IBook[]>>
+  searchForBook: () => void
 }
 
 const initialContext = {
   query: '',
-  setQuery: (query: string) => {},
+  setQuery: (newQuery: string) => {},
   results: [],
-  setResults: (results: IBook[]) => {},
+  setResults: (newResults: IBook[]) => {},
+  searchForBook: () => {},
 } as ISearchContext
 
 interface ISearchContextProviderProps {
@@ -33,8 +36,20 @@ export const SearchContextProvider = ({
   const [query, setQuery] = useState<string>('')
   const [results, setResults] = useState<IBook[]>([])
 
+  const searchForBook = async () => {
+    await performSearch(query).then((books) => setResults(books))
+  }
+
   return (
-    <SearchContext.Provider value={{ query, setQuery, results, setResults }}>
+    <SearchContext.Provider
+      value={{
+        query,
+        setQuery,
+        results,
+        setResults,
+        searchForBook: searchForBook,
+      }}
+    >
       {children}
     </SearchContext.Provider>
   )
