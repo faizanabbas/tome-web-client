1c62891b085217f2b54db8a23ccffb1ccf484704
Faizan Abbas <faizan@faizanabbas.me>
2023-03-05 17:28:40 +0000

    Add heading for bookshelf

--- src/components/Bookshelf/Bookshelf.tsx
+++ src/components/Bookshelf/Bookshelf.tsx
@@ -5,15 +5,31 @@ import BookshelfItem from './BookshelfItem'
 export default function Bookshelf() {
   const { bookshelf } = useContext(BookshelfContext)
 
+  let bookshelfHeadingText =
+    bookshelf.length > 0
+      ? 'Your bookshelf'
+      : 'Your bookshelf is looking empty...'
+
   if (bookshelf.length > 0) {
     return (
-      <ul className="space-y-2 border rounded p-2">
-        {bookshelf.map((book) => (
-          <BookshelfItem key={book.ISBN} book={book} />
-        ))}
-      </ul>
+      <>
+        <h1 className="text-4xl font-serif mt-12 mb-1">
+          {bookshelfHeadingText}
+        </h1>
+        <ul className="space-y-2 border rounded p-2">
+          {bookshelf.map((book) => (
+            <BookshelfItem key={book.ISBN} book={book} />
+          ))}
+        </ul>
+      </>
     )
   } else {
-    return <></>
+    return (
+      <>
+        <h1 className="text-4xl font-serif mt-12 mb-1">
+          {bookshelfHeadingText}
+        </h1>
+      </>
+    )
   }
 }
